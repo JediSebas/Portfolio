93203675e913a2091b0504c53091b0def7c4dad5
JediSebas <sebasdolata04@gmail.com>
2022-12-23 15:45:03 +0100

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,30 @@
+$("#me").click(function () {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#about-me").offset().top
+    }, 700);
+});
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    const scrollBtn = document.getElementById("scrollBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+}
+
+$("#scrollBtn").click(function () {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: document.body.scrollTop
+    }, 700);
+});
+
+$(document).ready(function () {
+    $('#menu-icon').click(function () {
+        $('nav ul').toggle('visible');
+    });
+});
